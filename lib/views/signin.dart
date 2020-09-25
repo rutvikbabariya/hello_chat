5e341b39d71cb6da7f038c4cf405a443ab94313c
Ajaykumar <ajay88491@gmail.com>
2020-09-25 21:48:41 +0530

    Fixes Sign-Out Bug

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -28,30 +28,34 @@ class _SignInState extends State<SignIn> {
   QuerySnapshot snapshotUserInfo;
 
   signIn(){
-    if(formKey.currentState.validate()){
+    if(formKey.currentState.validate()) {
+      HelperFunctions.saveUserEmailSharedPreference(
+          emailTextEditingController.text.trim());
 
-      HelperFunctions.saveUserEmailSharedPreference(emailTextEditingController.text);
-
-      databaseMethods.getUserByUserEmail(emailTextEditingController.text)
-          .then((val){
+      databaseMethods
+          .getUserByUserEmail(emailTextEditingController.text.trim())
+          .then((val) {
         snapshotUserInfo = val;
-        HelperFunctions
-            .saveUserNameSharedPreference(snapshotUserInfo.documents[0].data["name"]);
-      //  print("${snapshotUserInfo.documents[0].data["name"]}");
+        HelperFunctions.saveUserNameSharedPreference(
+            snapshotUserInfo.documents[0].data["name"]);
+        //  print("${snapshotUserInfo.documents[0].data["name"]}");
       });
-
 
       setState(() {
         isLoading = true;
       });
 
-      authMethods.signInWithEmailAndPassword(emailTextEditingController.text, passwordTextEditingController.text).then((val){
-
-        if(val != null){
+      authMethods
+          .signInWithEmailAndPassword(emailTextEditingController.text.trim(),
+              passwordTextEditingController.text.trim())
+          .then((val) {
+        if (val != null) {
           HelperFunctions.saveUserLoggedInSharedPreference(true);
-          Navigator.pushReplacement(context, MaterialPageRoute(
-            builder: (context) => ChatRoom(),
-          ));
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatRoom(),
+              ));
         }
       });
     }
@@ -158,7 +162,7 @@ class _SignInState extends State<SignIn> {
                       },
                       child: Container(
                         padding:  EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(" Register now",style: TextStyle(
+                        child: Text("Register now", style: TextStyle(
                           color: Colors.white,
                           fontSize: 17,
                           decoration: TextDecoration.underline,
